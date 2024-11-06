d3fa801efe5b18ab147a28b7ff173735557aff3a
Oliver Walters <oliver.henry.walters@gmail.com>
2024-11-06 01:10:18 +0000

    Add basic project setup files

---
+++ scheduling/core.py
@@ -0,0 +1,33 @@
+"""Stock scheduling plugin for InvenTree."""
+
+from plugin import InvenTreePlugin
+from plugin.mixins import SettingsMixin, UrlsMixin, UserInterfaceMixin
+
+from .version import PLUGIN_VERSION
+
+class StockSchedulingPlugin(SettingsMixin, UrlsMixin, UserInterfaceMixin, InvenTreePlugin):
+    """Stock scheduling plugin for InvenTree."""
+
+    AUTHOR = "Oliver Walters"
+    DESCRIPTION = "Stock scheduling plugin for InvenTree"
+    VERSION = PLUGIN_VERSION
+
+    MIN_VERSION = '0.17.0'
+
+    NAME = "Stock Scheduling"
+    SLUG = "scheduling"
+    TITLE = "Stock Scheduling Plugin"
+
+    SETTINGS = {}
+
+    def setup_urls(self):
+        """Returns the URLs defined by this plugin."""
+
+        # TODO: Define the view class for the scheduling view
+        return []
+
+    def get_ui_panels(self, request, context=None, **kwargs):
+        """Return the UI panels for the scheduling plugin."""
+        
+        # TODO: Define the UI panels for the scheduling plugin
+        return []
---
+++ scheduling/version.py
@@ -0,0 +1,3 @@
+"""Version information for the Scheduling plugin."""
+
+PLUGIN_VERSION = "0.1.0"
